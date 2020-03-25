4df3762d468cc2064a804a623d62e95fee51a393
No-YE <nye7181@gmail.com>
2020-03-25 17:35:49 +0900

    post upload cli 작성

---
+++ client/upload.go
@@ -0,0 +1,41 @@
+package client
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"os"
+)
+
+// Post -
+type Post struct {
+	Title      string   `json:"title"`
+	Categories []string `json:"categoryNames"`
+	Content    string   `json:"content"`
+}
+
+// Upload upload to BoN
+func Upload(post Post, token string) {
+	url := "https://www.noye.xyz/api/v1/post"
+
+	pByte, _ := json.Marshal(post)
+	buff := bytes.NewBuffer(pByte)
+
+	req, err := http.NewRequest("POST", url, buff)
+	if err != nil {
+		println(err)
+		os.Exit(1)
+	}
+
+	req.Header.Add("Content-Type", "application/json")
+	req.Header.Add("token", token)
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		println(err)
+		os.Exit(1)
+	}
+
+	println(resp.StatusCode)
+}
--- cmd/upload.go
+++ cmd/upload.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"github.com/No-YE/BoN-cli/client"
+	"github.com/No-YE/BoN-cli/keystore"
 	"github.com/spf13/cobra"
 )
 
@@ -10,7 +12,10 @@ var (
 		Short: "upload markdown file",
 		Long:  `upload markdown file to BoN with token`,
 		Run: func(cmd *cobra.Command, args []string) {
-			println(categories[0])
+			post := client.Post{title, categories, "test"}
+			token := keystore.GetToken()
+
+			client.Upload(post, token)
 		},
 	}
 	title      string
@@ -19,7 +24,7 @@ var (
 
 func init() {
 	uploadCmd.Flags().StringVarP(&title, "title", "t", "", "title (required)")
-	uploadCmd.Flags().StringArrayVarP(&categories, "categories", "c", []string{"abc"}, "categories")
+	uploadCmd.Flags().StringSliceVarP(&categories, "categories", "c", []string{}, "categories")
 	uploadCmd.MarkFlagRequired("title")
 
 	rootCmd.AddCommand(uploadCmd)
